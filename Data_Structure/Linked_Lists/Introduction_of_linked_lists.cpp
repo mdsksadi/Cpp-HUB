5495ab4058d79fc68d7ccfca477da7d15a317ae1
mdsksadi <sadiewu12345@gmail.com>
2022-12-29 22:29:42 +0100

    README.md file added for linked lists

--- Data_Structure/Linked_Lists/Introduction_of_linked_lists.cpp
+++ Data_Structure/Linked_Lists/Introduction_of_linked_lists.cpp
@@ -3,28 +3,6 @@
 #        sadiewu12345@gmail.com        #
 #######################################*/
 
-/*
-    Linked list is a non continuous collection
-    Linked list consists 2 thing:
-        1. Value
-        2. Address of the next element
-    Address of last element store be NULL
-    1st element of a linked list is called "head"
-
-    Advantage of linked list:
-    Array is fixed size but linked list is dinamic size. So, I can add new element or remove existing element.
-
-    Disadvantage of inked list:
-    Random access to the element of a linkd list is not allowed. 
-        Because you need the address of the element and the address is stored to the previous element. 
-        So, always need to start from the 1st (head) element.
-    Linked list needs more memory. 
-        Because linked list needs to store 2 things. 1. Value of the element and 2. Pointer of the next element. 
-        So, it needs more storage.
-
-    Linked list data tipe is always user defined
-    */
-
 #include<iostream>
 
 using namespace std;
---
+++ Data_Structure/Linked_Lists/README.md
@@ -0,0 +1,27 @@
+# Basic Instruction
+
+Linked list is a non continuous collection
+Linked list consists 2 thing:
+* Value. 
+* Address of the next element.
+
+Address of last element store be NULL
+1st element of a linked list is called "head"
+
+# Advantage of linked list:
+Array is fixed size but linked list is dinamic size. So, I can add new element or remove existing element.
+
+# Disadvantage of linked list:
+Random access to the element of a linkd list is not allowed.
+  * Because you need the address of the element and the address is stored to the previous element.
+  * So, always need to start from the 1st (head) element.
+   
+Linked list needs more memory.
+
+Because linked list needs to store 2 things.
+  1. Value of the element and 
+  2. Pointer of the next element.
+  
+  So, it needs more storage.
+
+Linked list data tipe is always user defined.
