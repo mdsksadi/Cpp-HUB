be604217fe244db0236c9257c164c3137c4ea44f
mdsksadi <sadiewu12345@gmail.com>
2022-12-30 00:22:44 +0100

    Introduction of STACK

---
+++ Data_Structure/STACK/Introduction_of_STACK.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+//Function for write out the element of the STACK
+void printStackElement(stack<int> stack)
+{
+    while (!stack.empty())
+    {
+        cout<<stack.top()<<endl;
+        stack.pop();
+    }
+    
+}
+
+
+int main()
+{   
+    //Create STACK to my program
+    stack<int>numbersStack;
+
+    //Add element to the STACK
+    numbersStack.push(5);
+    numbersStack.push(3);
+    numbersStack.push(2);
+    numbersStack.push(7);
+
+    //Remove element from the STACK
+    numbersStack.pop();
+
+    //Check if the STACK is empty or not
+    if (numbersStack.empty())
+        cout<<"Stack is empty"<<endl;
+    else
+        cout<<"Stack is not empty"<<endl;
+
+    //Size of the STACK
+    cout<<"Stack size is: "<<numbersStack.size()<<endl;
+
+    //Write out the element of the STACK
+    cout<<"The Stack is:"<<endl;
+    printStackElement(numbersStack);
+
+
+    return 0;
+}
---
+++ Data_Structure/STACK/README.md
@@ -0,0 +1,20 @@
+# What is STACK?
+* STACK is a type of data collection which use LIFO (Last in first out) technique in order to store element. We can also say:
+  * FILO (First in last out)
+  * LCFS (Last come first serve)
+  
+# Where do we need this type of data collection?
+* Redo and Undo
+* Back and Forward button 
+  
+# 5 most important functions that are associated with STACK
+  1. Function that will say, the STACK is empty or not (Empty)
+  2. Function that will say the size of the STACK (Size)
+  3. Function that will give the top element of the STACK (Top)
+  4. Function that add element into the STACK (Push)
+  5. Function to remeove element from the STACK (Pop)
+
+# Steps:
+  1. include "stack" header file.
+  2. stack<type of element (eg. int)>numbersStack(name of the stack)
+  3. 
